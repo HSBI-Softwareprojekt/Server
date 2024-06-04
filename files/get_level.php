e8abf52893cbbb26e6ab9b2a9f3ee0103b88b52a
Alexey Khokhlov <alexey.khokhlov@fh-bielefeld.de>
2024-06-04 19:02:27 +0200

    Updated get_level.php for client submenue

--- files/get_level.php
+++ files/get_level.php
@@ -1,34 +1,38 @@
 <?php
-if(isset($_POST["DATA"])) {
+if(isset($_POST["ID"])) {
 	include('./db.php');
 	$error = false;
 	$msg["error"] = array();
 	$data = array();
 
-	$post = JSON_DECODE($_POST["DATA"], true);
-	if(strlen($post["PLAYER"]) == 0) {
+	$id = trim($_POST["ID"]);
+	
+	if(strlen($id) == 0) {
 		$error = true;
 		$msg["error"][] = "Übertragungs Fehler, kein Spieler";
-	} else if(!is_numeric($post["PLAYER"])) {
+	} else if(!is_numeric($id)) {
 		$error = true;
 		$msg["error"][] = "Übertragungs Fehler, Spieler hat eine ungültige ID";
 	} else {
-		$sql = "SELECT id FROM users WHERE id = ".intval($post["PLAYER"]);
+		$sql = "SELECT id FROM users WHERE id = ".intval($id);
 		$result = $pdo->query($sql)->fetch();
 		if(!$result) {
 			$error = true;
 			$msg["error"][] = "Spieler existiert nicht";
 		} else {
-			$post["PLAYER"] = intval($post["PLAYER"]);
+			$id = intval($id);
 		}
 	}
 	
 	if(!$error) {
-		$sql = "SELECT user_id, level FROM level WHERE user_id = ".$post["PLAYER"]." ORDER BY level ASC";
+		$sql = "SELECT user_id, level FROM level WHERE user_id = ".$id." ORDER BY level ASC";
 		$res = mysqli_query($mysqli, $sql);
 		while($row = mysqli_fetch_assoc($res)) {
-			$data[] = intval($row["level"]);
+			$data["level"][] = intval($row["level"]);
 		} mysqli_free_result($res);
+		if(count($data) == 0) {
+			$data["level"][] = 0;
+		}
 	}
 	
 	if(!$error) {
